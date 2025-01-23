9690a4951f1aa57454f06d9ddf2085c06feb8e2a
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-01-23 07:29:38 +0500

    new

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
+// import Link from "next/link";
 import { AuthProvider } from "@/context/AuthContext";
 import Navigation from "@/components/Navigation";
 
