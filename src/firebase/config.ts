a616d97be9e431f4599e09866d4c69e18288004d
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-01-23 08:00:58 +0500

    configs

---
+++ src/firebase/config.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCbI7KoCRnEk5BfbYWVRiMX7VkaAmdqvqw",
+  authDomain: "nextjob-0307.firebaseapp.com",
+  projectId: "nextjob-0307",
+  storageBucket: "nextjob-0307.firebasestorage.app",
+  messagingSenderId: "150845008312",
+  appId: "1:150845008312:web:969c07a13e5dd0f7aec74d",
+};
+
+const app = initializeApp(firebaseConfig);
+export default app;
